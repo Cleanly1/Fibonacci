1382176af1464bf1572bcaf8670f2a37d8e5ffd0
Cleanly <oskar.turesson99@gmail.com>
2019-10-02 00:22:26 +0200

    Several small fixes and changes

---
+++ header.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="styleFib.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Start</title>
+</head>
+<body>
+  <header>
+  <div class="headStyle">
+    <h1>The Fibonacci Numbers</h1>
+    </div>
+  </header>
