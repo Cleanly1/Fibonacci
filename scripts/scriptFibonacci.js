1f0a2bd281cb99049c81165577c35f7fb849a12d
Cleanly <oskar.turesson99@gmail.com>
2020-10-16 23:23:25 +0200

    updated js function assignment

--- scripts/scriptFibonacci.js
+++ scripts/scriptFibonacci.js
@@ -3,28 +3,32 @@
 // window.addEventListener("onload", runningCheck());
 // var isMadnessRunning = new Boolean(true);
 
-function madness() {
-    var allButtons = document.querySelectorAll(".rainbowButton");
-    document.body.style.animation = "backg 1s infinite";
-    document.querySelector(".headStyle").style.animation = "colorT 1s infinite";
+const allButtons = document.querySelectorAll(".button");
+const header = document.querySelector(".headStyle");
+const inputField = document.querySelector("#inputField");
+const rainbowButtons = document.querySelectorAll(".rainbow");
+
+rainbowButtons.forEach(() => {});
+
+const madness = () => {
+    document.body.classList.add("animateBackground");
+    header.classList.add("animateText");
     for (var i = 0; i < allButtons.length; i++) {
         allButtons[i].style.animation = "backg 0.5s infinite";
     }
-    window.document.querySelector("#inputField").style.animation =
-        "backg 1s infinite";
-}
+    inputField.style.animation = "backg 1s infinite";
+};
 
-function stopMadness() {
-    var allButtons = document.querySelectorAll(".rainbowButton");
-    document.body.style.animation = "paused";
-    document.querySelector(".headStyle").style.animation = "paused";
+const stopMadness = () => {
+    document.body.classList.remove("animateBackground");
+    header.classList.remove("animateText");
     for (var i = 0; i < allButtons.length; i++) {
         allButtons[i].style.animation = "paused";
     }
-    document.querySelector("#inputField").style.animation = "paused";
-}
+    inputField.style.animation = "paused";
+};
 
-const randomBackground = function() {
+const randomBackground = () => {
     const rgb1 = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
     const rgb2 = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
     const rgb3 = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
--- views/home.php
+++ views/home.php
@@ -4,11 +4,11 @@
 <div class="form">
     <form action="/views/result.php" method="get">
         <input name="number" type="number" id="inputField">
-        <button class="rainbowButton button" type="submit">Submit</button>
+        <button class="button" type="submit">Submit</button>
 
     </form>
-    <button class="button rainbowButton rainbow" onclick="madness()">Click to start the rainbow</button>
-    <button class="button rainbowButton rainbow" onclick="stopMadness()">Click to stop the rainbow</button>
+    <button class="button rainbow" onclick="madness()">Click to start the rainbow</button>
+    <button class="button rainbow" onclick="stopMadness()">Click to stop the rainbow</button>
 </div>
 
 
