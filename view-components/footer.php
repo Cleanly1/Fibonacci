d2aea38544974b58e7cb80f65e4b6f191346ae26
Cleanly <oskar.turesson99@gmail.com>
2021-10-07 22:32:59 +0200

    added updated header and footer files

--- view-components/footer.php
+++ view-components/footer.php
@@ -1,4 +1,4 @@
-        <script type="text/javascript" src="../scripts/scriptFibonacci.js">
+        <script type="text/javascript" src="../dist/script/script.js">
 
         </script>
         </body>
--- view-components/header.php
+++ view-components/header.php
@@ -9,13 +9,12 @@ require __DIR__ . "/../functions.php";
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../assets/FibonacciSpiral.png" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" href="../styles/styleFib.css">
+    <link rel="stylesheet" type="text/css" href="../dist/style/style.css">
     <title>Start</title>
 </head>
 
 <body>
     <header>
-        <div class="headStyle">
-            <h1>The Fibonacci Numbers</h1>
-        </div>
+        <h1>The Fibonacci Numbers</h1>
+        <nav class="navbar"><a class="button navButton" href="/views/home.php">Home</a></nav>
     </header>
