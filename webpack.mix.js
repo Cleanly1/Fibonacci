2b23d644dd8c765c943e009b23eb74878f06bb72
Cleanly <oskar.turesson99@gmail.com>
2021-10-13 08:00:40 +0200

    fixed indentation for files

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,11 @@
 let mix = require("laravel-mix");
 
 if (mix.inProduction()) {
-    mix.sass("styles/style.scss", "dist/style/style.css").js(
-        "scripts/*",
-        "dist/script/script.js"
-    );
+	mix
+		.sass("styles/style.scss", "dist/style/style.css")
+		.js("scripts/*", "dist/script/script.js");
 } else {
-    mix.sass("styles/style.scss", "dist/style/style.css").js(
-        "scripts/*.js",
-        "dist/script/script.js"
-    );
+	mix
+		.sass("styles/style.scss", "dist/style/style.css")
+		.js("scripts/*.js", "dist/script/script.js");
 }
