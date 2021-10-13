f06acd391dd65277a62243822773c8ee2340aa25
Cleanly <oskar.turesson99@gmail.com>
2021-10-13 07:57:26 +0200

    added text shrinking on scroll

--- scripts/scriptFibonacci.js
+++ scripts/scriptFibonacci.js
@@ -1,65 +1,70 @@
-// Might add this feature later
-// window.addEventListener("scroll", madness);
-// window.addEventListener("onload", runningCheck());
-
 let isMadnessRunning;
-let interval;
 const allButtons = document.querySelectorAll(".button");
 const header = document.querySelector(".headStyle");
 const inputField = document.querySelector("#inputField");
 const rainbowButton = document.querySelector(".rainbow");
 
 rainbowButton.addEventListener("click", () => {
-    if (!isMadnessRunning) {
-        madness();
-    } else {
-        stopMadness();
-    }
+	if (!isMadnessRunning) {
+		madness();
+	} else {
+		stopMadness();
+	}
 });
 
 rainbowButton.addEventListener("mouseenter", (e) => {
-    rainbowButton.classList.add("animateButtons");
+	rainbowButton.classList.add("animateButtons");
 });
 
 rainbowButton.addEventListener("mouseleave", (e) => {
-    if (!isMadnessRunning) {
-        rainbowButton.classList.remove("animateButtons");
-    }
+	if (!isMadnessRunning) {
+		rainbowButton.classList.remove("animateButtons");
+	}
 });
 
 const madness = () => {
-    isMadnessRunning = !isMadnessRunning;
-    document.body.classList.add("animateBackground");
-    header.classList.add("animateText");
-    for (var i = 0; i < allButtons.length; i++) {
-        allButtons[i].classList.add("animateButtons");
-    }
-    inputField.classList.add("animateBackground");
+	isMadnessRunning = !isMadnessRunning;
+	document.body.classList.add("animateBackground");
+	header.classList.add("animateText");
+	for (var i = 0; i < allButtons.length; i++) {
+		allButtons[i].classList.add("animateButtons");
+	}
+	inputField.classList.add("animateBackground");
 };
 
 const stopMadness = () => {
-    isMadnessRunning = !isMadnessRunning;
-    document.body.classList.remove("animateBackground");
-    header.classList.remove("animateText");
-    for (var i = 0; i < allButtons.length; i++) {
-        allButtons[i].classList.remove("animateButtons");
-    }
-    inputField.classList.remove("animateBackground");
+	isMadnessRunning = !isMadnessRunning;
+	document.body.classList.remove("animateBackground");
+	header.classList.remove("animateText");
+	for (var i = 0; i < allButtons.length; i++) {
+		allButtons[i].classList.remove("animateButtons");
+	}
+	inputField.classList.remove("animateBackground");
 };
 
-const randomBackground = () => {
-    const rgb1 = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-    const rgb2 = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-    const rgb3 = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-    if (window.scrollY > 500) {
-        document.body.style.backgroundColor = `rgb( ${rgb1}, ${rgb2}, ${rgb3})`;
-    } else {
-        document.body.style.backgroundColor = `rgb( 143, 188, 143)`;
-    }
-};
+// const randomBackground = () => {
+//     const rgb1 = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+//     const rgb2 = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+//     const rgb3 = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+//     if (window.scrollY > 500) {
+//         document.body.style.backgroundColor = `rgb( ${rgb1}, ${rgb2}, ${rgb3})`;
+//     } else {
+//         document.body.removeAttribute("style");
+//     }
+// };
 
 window.addEventListener("scroll", () => {
-    if (!interval) {
-        interval = setInterval(randomBackground, 1000);
-    }
+	let resultText = document.querySelectorAll(".spans");
+
+	for (let i = resultText.length - 1; i >= 0; i--) {
+		const element = resultText[i];
+		let fontSize = i;
+
+		if (fontSize > 250) {
+			fontSize = 10.5 - i / 200;
+		} else {
+			fontSize = 12 - i / 100;
+		}
+		element.setAttribute("style", `font-size:${fontSize}px;`);
+	}
 });
