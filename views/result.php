2ab3065309609384200a2a0095b9d49a253ad014
Cleanly <oskar.turesson99@gmail.com>
2021-10-19 20:19:42 +0200

    cleaned up the code

--- views/result.php
+++ views/result.php
@@ -1,14 +1,20 @@
 <?php require __DIR__ . "/../view-components/header.php";
 
 $number = filter_var($_GET["number"], FILTER_SANITIZE_NUMBER_INT);
+$result = calculateFib($number);
 
 ?>
 
-<a class="backButton button" href="/views/home.php">Want new numbers?</a>
+<a class="button backButton" href="/views/home.php">Want new numbers?</a>
 
 <div class="result">
-    <?php require __DIR__ . '/../view-components/post.php'; ?>
-    <?php echo calculateFib($number); ?>
+	<?php require __DIR__ . '/../view-components/post.php'; ?>
+
+	<?php foreach ($result as $value) : ?>
+		<span class="spans">
+			<?= $value  ?>
+		</span>
+	<?php endforeach; ?>
 </div>
 
 
